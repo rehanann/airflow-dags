b0d1579555c24a387bfc6ca8c501e75e471409ca
Rehan Anwar <rehananwar@Rehans-MacBook-Pro-2.local>
2025-06-09 13:23:42 +0100

    fixing dags

--- deploy_spark_chart_bashoperator.py
+++ deploy_spark_chart_bashoperator.py
@@ -8,28 +8,29 @@ default_args = {
     'retries': 1,
 }
 
-with DAG(
+dag = DAG(  # ← Assign DAG to variable so Airflow can detect it
     dag_id='deploy_spark_chart_bashoperator',
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
     tags=['helm', 'spark'],
-) as dag:
+)
 
-    deploy_chart = BashOperator(
-        task_id='deploy_spark_chart',
-        bash_command="""
-        curl -X POST \
-          http://helm-api-api.default.svc.cluster.local:8000/install \
-          -H 'accept: application/json' \
-          -H 'Content-Type: application/json' \
-          -d '{
-            "release_name": "mytest",
-            "chart_name": "spark-chart/spark-chart",
-            "namespace": "test",
-            "values": {
-              "additionalProp1": {}
-            }
-          }'
-        """
-    )
+deploy_chart = BashOperator(
+    task_id='deploy_spark_chart',
+    bash_command="""
+    curl -X POST \
+      http://helm-api-api.default.svc.cluster.local:8000/install \
+      -H 'accept: application/json' \
+      -H 'Content-Type: application/json' \
+      -d '{
+        "release_name": "mytest",
+        "chart_name": "spark-chart/spark-chart",
+        "namespace": "test",
+        "values": {
+          "additionalProp1": {}
+        }
+      }'
+    """,
+    dag=dag,  # ← Ensure task is bound to DAG
+)
